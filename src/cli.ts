59aa3aee9bb8106d53753a1e2e276950140e1abe
mortalYoung <yangwei1@outlook.com>
2022-07-20 17:03:36 +0800

    feat: support create typescript project

--- src/cli.ts
+++ src/cli.ts
@@ -4,51 +4,79 @@ import { program } from "commander";
 import { join, resolve } from "path";
 
 let projectName = "";
+let isTypeScript = false;
+
+function isUsingYarn() {
+  return (process.env.npm_config_user_agent || "").indexOf("yarn") === 0;
+}
 
 function init() {
   program
     .command("create-molecule")
     .arguments("<project-directory>")
-    .action((name) => {
+    .option("-t --typescript", "create an TypeScript Project")
+    .action((name, options) => {
       projectName = name;
+      isTypeScript = options?.typescript || false;
     })
     .parse(process.argv);
 
-  execSync(`pnpm create react-app ${projectName}`, {
-    stdio: "inherit",
-  });
+  const useYarn = isUsingYarn();
+
+  execSync(
+    `${useYarn ? "yarn create" : "npx"} create-react-app ${projectName} ${
+      isTypeScript ? "--template typescript" : ""
+    }`,
+    {
+      stdio: "inherit",
+    }
+  );
 
   const projectPath = resolve(projectName);
-  execSync("pnpm add @dtinsight/molecule@latest", {
+  execSync(`${useYarn ? "yarn" : "npm"} add @dtinsight/molecule@latest`, {
     cwd: projectPath,
     stdio: "inherit",
   });
 
   const forked = fork(join(__dirname, "file.js"));
-  forked.send({ cwd: projectPath });
+  forked.send({ cwd: projectPath, typescript: isTypeScript });
 
   console.log(
     `${chalk.greenBright(
       "Success"
     )}! Created ${projectName} at ${projectPath} by create-react-app`
   );
-  console.log("We rewrite several files:");
+  console.log("We rewrote several files:");
   console.log("");
-  console.log(` * ${join(projectPath, "src", "App.js")} ${chalk.cyan("and")}`);
-  console.log(` * ${join(projectPath, "src", "index.js")}.`);
+  console.log(
+    ` * ${join(
+      projectPath,
+      "src",
+      isTypeScript ? "App.tsx" : "App.js"
+    )} ${chalk.cyan("and")}`
+  );
+  console.log(
+    ` * ${join(projectPath, "src", isTypeScript ? "index.tsx" : "index.js")}.`
+  );
   console.log("");
 
   console.log("You can begin by typing:");
   console.log("");
   console.log(`     ${chalk.cyan("cd")} my-app`);
-  console.log(`     ${chalk.cyan("yarn")} start`);
+  console.log(`     ${chalk.cyan(useYarn ? "yarn" : "npm run")} start`);
   console.log("");
 
   console.log("Or if you want to review the diff of lines:");
   console.log("");
   console.log(`     ${chalk.cyan("cd")} my-app`);
-  console.log(`     ${chalk.cyan("git diff src/App.js")}`);
-  console.log(`     ${chalk.cyan("git diff src/index.js")}`);
+  console.log(
+    `     ${chalk.cyan(`git diff src/${isTypeScript ? "App.tsx" : "App.js"}`)}`
+  );
+  console.log(
+    `     ${chalk.cyan(
+      `git diff src/${isTypeScript ? "index.tsx" : "index.js"}`
+    )}`
+  );
   console.log("");
   console.log("Happy coding!😊");
 }
--- src/file.ts
+++ src/file.ts
@@ -2,19 +2,6 @@ import { readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 import { exit } from "process";
 
-process.on("message", (msg: any) => {
-  const cwd = msg.cwd;
-  writeFileSync(join(cwd, "src", "App.js"), content);
-
-  const indexFile = readFileSync(join(cwd, "src", "index.js"), "utf-8");
-
-  const nextFile = indexFile.replace(/React.StrictMode/g, "");
-
-  writeFileSync(join(cwd, "src", "index.js"), nextFile);
-
-  exit(1);
-});
-
 const content = `import React from 'react';
 import { create, Workbench } from '@dtinsight/molecule';
 import '@dtinsight/molecule/esm/style/mo.css';
@@ -26,3 +13,25 @@ const moInstance = create({
 const App = () => moInstance.render(<Workbench />);
 
 export default App;`;
+
+process.on("message", (msg: { cwd: string; typescript: boolean }) => {
+  const cwd = msg.cwd;
+  writeFileSync(
+    join(cwd, "src", msg.typescript ? "App.tsx" : "App.js"),
+    content
+  );
+
+  const indexFile = readFileSync(
+    join(cwd, "src", msg.typescript ? "index.tsx" : "index.js"),
+    "utf-8"
+  );
+
+  const nextFile = indexFile.replace(/React.StrictMode/g, "");
+
+  writeFileSync(
+    join(cwd, "src", msg.typescript ? "index.tsx" : "index.js"),
+    nextFile
+  );
+
+  exit(1);
+});
